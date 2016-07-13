f206663c02d18e7bb1f531c2941b7160c08e2fad
mrlabpc <mrlabpc@outlook.com>
2016-07-13 17:26:21 -0400

    Models in Vive scene

--- Assets/Holojam/Dollhouse-Vive/ViveHolojamViewManager.cs
+++ Assets/Holojam/Dollhouse-Vive/ViveHolojamViewManager.cs
@@ -8,6 +8,22 @@ namespace Holojam {
 		public Network.HolojamView ViveWandRight;
 		// Use this for initialization
 		void Start () {
+			
+		}
+		
+		// Update is called once per frame
+		void Update () {
+			ViveHeadset.RawPosition = ViveHeadset.transform.position;
+			ViveHeadset.RawRotation= ViveHeadset.transform.rotation;
+
+			ViveWandLeft.RawPosition = ViveWandLeft.transform.position;
+			ViveWandLeft.RawRotation= ViveWandLeft.transform.rotation;
+
+			ViveWandRight.RawPosition = ViveWandRight.transform.position;
+			ViveWandRight.RawRotation= ViveWandRight.transform.rotation;
+		}
+
+		void Awake () {
 			ViveHeadset.Label = "vive";
 			ViveWandLeft.Label = "vive_controller_left";
 			ViveWandRight.Label = "vive_controller_right";
@@ -15,11 +31,6 @@ namespace Holojam {
 			ViveHeadset.IsMine = true;
 			ViveWandLeft.IsMine = true;
 			ViveWandRight.IsMine = true;
-		}
-		
-		// Update is called once per frame
-		void Update () {
-		
 		}
 	}
 }
